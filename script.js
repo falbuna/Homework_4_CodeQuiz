384aab8de9480419614e5db82c488abde6794a33
Francis Albuna <falbuna1@gmail.com>
2020-07-26 10:11:35 -0700

    Added placeholder content for the first question of the quiz.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var startButton = document.querySelector("#start");
-var quizElement = document.querySelector(".main");
-var bodyEl = document.createElement('div');
+const startButton = document.querySelector('#start');
+const quizElement = document.querySelector('.main');
+const bodyEl = document.createElement('div');
 
+const promptElement = document.querySelector('#prompt');
+const quizPrompt = document.createElement('h1');
 
 // This is for the Timer
 var startTime = 60;
@@ -16,17 +18,21 @@ function startQuiz() {
 
         secondsElapsed++;
 
-        if(totalTime <= 0) {
-            // stop
+        if(totalTime === 0) {
+            function stopTimer(){
+                clearInterval(startQuiz);
+            }
         }
 
     }, 1000);
 
+    // The First Prompt of the Quiz
+    promptElement.appendChild(quizPrompt);
+    promptElement.textContent = "First Question";
+
     quizElement.append(bodyEl);
-    quizElement.textContent = "First Question";
+    quizElement.textContent = "Who What Where Why";
 
 }
-
-
 
 startButton.addEventListener("click", startQuiz);
