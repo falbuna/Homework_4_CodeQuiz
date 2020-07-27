6927af622d662d47c08731ee69663ae78ecaa813
Francis Albuna <falbuna1@gmail.com>
2020-07-26 18:25:59 -0700

    Added 2nd quiz question. Edited layout to try and render the 2nd question.

--- script.js
+++ script.js
@@ -4,18 +4,24 @@ const bodyEl = document.createElement('div');
 const promptElement = document.querySelector('#prompt');
 const quizPrompt = document.createElement('h1');
 
-const prompt1 = ['How many versions of Bootstrap have been released?']
-const answers1 = ['1. 1', '2. 2', '3. 3', '4. 4'];
+
+const prompt1 = {
+    'Question': 'How many versions of Bootstrap have been released?',
+    'Choices': ['1. 1', '2. 2', '3. 3', '4. 4']}
+
+const prompt2 = {
+    'Question': 'Which company developed Javascript?',
+    'Choices': ['1. Netscape', '2. Google', '3. Microsoft', '4. Amazon']}
+
 
 // This is for the Timer
-var startTime = 60;
+var startTime = parseInt(60);
 var secondsElapsed = 0;
-var interval;
 
 function startQuiz() {
 
     setInterval(function () {
-        const totalTime = startTime - secondsElapsed;
+        const totalTime = parseInt(startTime - secondsElapsed);
         document.getElementById('timer').innerText = 'Timer: ' + totalTime;
 
         secondsElapsed++;
@@ -23,48 +29,39 @@ function startQuiz() {
         if(totalTime === 0) {
             function stopTimer(){
                 clearInterval(startQuiz);
+                alert("Time is up!");
             }
         }
 
     }, 1000);
 
-
-
     // The First Prompt of the Quiz
 
-    promptElement.appendChild(quizPrompt);
-    promptElement.textContent = "First Question";
+    renderQuiz();
 
-    renderQuestion();
+    function renderQuiz(){
 
-    function renderQuestion(){
-
-        const question = prompt1;
+        const question = prompt1['Question'];
         promptElement.innerHTML = question;
         promptElement.appendChild(quizPrompt);
 
-    }
-
-    renderQuestion();
-
-
-    renderAnswers();
-
-    function renderAnswers(){
-
         quizElement.innerHTML = '';
-        for (let i = 0; i <answers1.length; i++){
-            const answers = answers1[i];
-            const li = document.createElement('li');
+        for (let i = 0; i < prompt1.Choices.length; i++){
+            var answers = prompt1.Choices[i];
+            var li = document.createElement('li');
             var button = document.createElement("button");
             button.textContent = answers;
             li.appendChild(button);
             quizElement.appendChild(li);
         }
+
     }
 
-    renderAnswers();
+    quizElement.addEventListener('click', function(event) {
+        console.log("I've Been Clicked!")});
     
+    renderQuiz();
+
 }
 
 startButton.addEventListener("click", startQuiz);
